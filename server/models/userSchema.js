74383bc0e95d9a6b4fa74277bd052b729d8644b5
DeepakS-Github <21310@iiitu.ac.in>
2024-11-25 22:52:55 +0530

    add: user schema

---
+++ server/models/userSchema.js
@@ -0,0 +1,28 @@
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'User'],
+        default: 'User'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('users', userSchema);
+
