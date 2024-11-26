b4816ac74fc0806c679d0b7c1d8d02d6e39eec84
DeepakS-Github <21310@iiitu.ac.in>
2024-11-26 18:41:43 +0530

    client-add: password criteria

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 
 
-const Auth = ({mode}) => {
+const Auth = ({ mode }) => {
 
     const [formDetails, setFormDetails] = useState({});
     const { isLoading, sendRequest } = useHttpClient();
@@ -14,7 +14,7 @@ const Auth = ({mode}) => {
 
 
     const handleAuth = async () => {
-        if (mode==="signup") {
+        if (mode === "signup") {
             const response = await sendRequest({
                 url: '/auth/signup',
                 method: 'POST',
@@ -42,13 +42,13 @@ const Auth = ({mode}) => {
 
     return (
         <section className="mx-auto my-24 w-1/3">
-            <h1 className="mb-8 text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">{mode==="signup" ? "Create Account" : "Login"}</h1>
+            <h1 className="mb-8 text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">{mode === "signup" ? "Create Account" : "Login"}</h1>
             <form className="flex flex-col" onSubmit={(e) => {
                 e.preventDefault();
                 handleAuth()
             }}>
                 <div className="grid gap-6 mb-2 md:grid-cols-1">
-                    {mode==="signup" && <div>
+                    {mode === "signup" && <div>
                         <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">Username</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Doe" required onChange={(e) => {
                             setFormDetails({
@@ -75,7 +75,7 @@ const Auth = ({mode}) => {
                             })
                         }} />
                     </div>
-                    {mode==="signup" && <div>
+                    {mode === "signup" && <div>
                         <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
                         <input type="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="•••••••••" required onChange={(e) => {
                             setFormDetails({
@@ -83,14 +83,16 @@ const Auth = ({mode}) => {
                                 confirmPassword: e.target.value
                             })
                         }} />
+                        <div className="text-xs font-semibold cursor-pointer text-red-700">*Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character (e.g., !@#$%^&*).</div>
                     </div>}
                 </div>
                 <div className="mb-3">
                     <span className="text-sm font-semibold cursor-pointer text-blue-700 hover:underline" onClick={() => {
-                        navigate(mode==="signup"?"/login":"/signup")
-                    }}>{mode==="signup" ? "Click here to login" : "Click here to create account"}</span>
+                        navigate(mode === "signup" ? "/login" : "/signup")
+                    }}>{mode === "signup" ? "Click here to login" : "Click here to create account"}</span>
                 </div>
-                <button type="submit" className="flex flex-row justify-center items-center text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">{isLoading && <Spinner width={"w-5"} color={"white"} />}{mode==="signup" ? "Create Account" : "Login"}</button>
+
+                <button type="submit" className="flex flex-row justify-center items-center text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">{isLoading && <Spinner width={"w-5"} color={"white"} />}{mode === "signup" ? "Create Account" : "Login"}</button>
             </form>
         </section>
 
