2f3c079836f9745aabf06d20a99c28e3cecb658e
DeepakS-Github <21310@iiitu.ac.in>
2024-11-26 18:34:59 +0530

    add: email validation

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,22 +1,26 @@
 const bcrypt = require("bcryptjs");
 const User = require('../models/user.model');
 const { generateAccessToken } = require("../utils/token.utils");
-const { isStrongPassword } = require("../utils/validation.utils");
+const { isStrongPassword, isValidEmail } = require("../utils/validation.utils");
 
 const signup = async (req, res) => {
     try {
         let { username, email, password, confirmPassword } = req.body;
 
-        if(!username || !email || !password || !confirmPassword) {
+        if (!username || !email || !password || !confirmPassword) {
             return res.status(400).json({ message: "Please fill in all fields" });
         }
 
-        if(!isStrongPassword(password)){
-            return res.status(400).json({ message: "Password must be follow the validation criteria"});
+        if (!isValidEmail(email)) {
+            return res.status(400).json({ message: "Invalid email" });
         }
 
-        if(password!=confirmPassword){
-            return res.status(400).json({message: "Password and Confirm Password do not match"})
+        if (!isStrongPassword(password)) {
+            return res.status(400).json({ message: "Password must be follow the validation criteria" });
+        }
+
+        if (password != confirmPassword) {
+            return res.status(400).json({ message: "Password and Confirm Password do not match" })
         }
 
         const salt = await bcrypt.genSalt(8);
@@ -51,7 +55,7 @@ const login = async (req, res) => {
     try {
         let { email, password } = req.body;
 
-        if(!email || !password) {
+        if (!email || !password) {
             return res.status(400).json({ message: "Please fill in all fields" });
         }
 
--- server/utils/validation.utils.js
+++ server/utils/validation.utils.js
@@ -1,14 +1,33 @@
-// Password Criteria:
-// 1. At least 8 characters long.
-// 2. Contains at least one lowercase letter.
-// 3. Contains at least one uppercase letter.
-// 4. Contains at least one numeric digit.
-// 5. Contains at least one special character (e.g., !@#$%^&*()).
 
 
 export const isStrongPassword = (password) => {
+    // Password Criteria:
+    // 1. At least 8 characters long.
+    // 2. Contains at least one lowercase letter.
+    // 3. Contains at least one uppercase letter.
+    // 4. Contains at least one numeric digit.
+    // 5. Contains at least one special character (e.g., !@#$%^&*()).
+
     const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
 
     return strongPasswordRegex.test(password);
+};
+
+
+
+export const isValidEmail = (email) => {
+
+    // Email Criteria:
+    // 1. Ensures at least one character exists before the @.
+    // 2. Ensures exactly one @ is present.
+    // 3. Ensures the domain name has at least one character before the ..
+    // 4. Ensures a . exists after the domain name.
+    // 5. Ensures the top-level domain (TLD) has at least one character.
+    // 6. Disallows spaces anywhere in the email.
+    // 7. Validates the basic structure: local-part@domain.tld.
+
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 };
 
