70e3f48310d543694a658746ea78430a096a713e
DeepakS-Github <21310@iiitu.ac.in>
2024-11-26 22:39:38 +0530

    server-add: cors options

--- server/index.js
+++ server/index.js
@@ -5,22 +5,28 @@ const cors = require('cors');
 
 const app = express();
 
+const corsOptions = {
+    origin: ["http://localhost:5173", "https://vrv-security-server.vercel.app"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 
 const auth = require('./routes/auth.route');
 const user = require('./routes/user.route');
 
-app.get('/health-check', (req, res)=>{
+app.get('/health-check', (req, res) => {
     res.status(200).send("Server is working");
 })
 
 app.use("/auth", auth);
 app.use("/user", user);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
