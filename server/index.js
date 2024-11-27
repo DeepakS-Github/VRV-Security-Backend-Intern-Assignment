b74e486ee1707f21ed8a82b7f213324721e6db10
DeepakS-Github <21310@iiitu.ac.in>
2024-11-27 15:45:54 +0530

    server-add: admin env variables and creating if not present in db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 require('./config/connectDB');
+require('./services/admin.service');
+
 const express = require('express');
 const cors = require('cors');
 
---
+++ server/services/admin.service.js
@@ -0,0 +1,34 @@
+
+// Create at max one admin if not present in database through the environeent admin credentials
+
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+
+const createAdmin = async () => {
+    try {
+        const admin = await User.findOne({ role: "Admin" });   
+        if (!admin) {
+            console.log("Admin not found, creating one...");
+
+            const { ADMIN_USERNAME, ADMIN_EMAIL, ADMIN_PASSWORD } = process.env;
+            if (!ADMIN_USERNAME || !ADMIN_EMAIL || !ADMIN_PASSWORD) {
+                throw new Error("Admin credentials not provided");
+            }
+
+            const salt = await bcrypt.genSalt(8);
+            const password = await bcrypt.hash(ADMIN_PASSWORD, salt);
+
+            await User.create({ username: ADMIN_USERNAME, email: ADMIN_EMAIL, password, role: "Admin" });
+            console.log("Admin created successfully");
+        }
+    }
+    catch (error) {
+        console.log("Something went wrong! while creating admin", error);
+        
+        // Stops the server if an error occurs while creating the admin
+        process.exit(1);
+    }
+}
+
+
+createAdmin();
